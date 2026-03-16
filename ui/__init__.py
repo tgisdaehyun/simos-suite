8c51d9597b543b5bc3f2b565d1b77277edfbc33e
dspl1236 <7971074+dspl1236@users.noreply.github.com>
2026-03-16 13:08:35 -0400

    ui: export TransLoggerTab

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,4 +1,5 @@
 from ui.interface_panel import InterfacePanel
 from ui.main_window import MainWindow, main
+from ui.trans_logger import TransLoggerTab
 
-__all__ = ["InterfacePanel", "MainWindow", "main"]
+__all__ = ["InterfacePanel", "MainWindow", "TransLoggerTab", "main"]
